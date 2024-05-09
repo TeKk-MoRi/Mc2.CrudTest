4816d0970310e70622c1b117cad1d0b372ee343f
TeKk_ MoRi <morteza.nejat966@gmail.com>
2024-05-09 12:28:19 +0330

    customer model

---
+++ Mc2.CrudTest.Domain/Models/Customer.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mc2.CrudTest.Domain.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [StringLength(100)]
+        public string Email { get; set; }
+
+        [Required]
+        [MinLength(10)]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [StringLength(20)] // Assuming a 20-digit bank account number
+        public string BankAccountNumber { get; set; }
+    }
+    public class CustomerConfiguration : IEntityTypeConfiguration<Customer>
+    {
+        public void Configure(EntityTypeBuilder<Customer> builder)
+        {
+            // Set up uniqueness constraints
+            builder.HasIndex(c => new { c.FirstName, c.LastName, c.DateOfBirth }).IsUnique();
+            builder.HasIndex(c => c.Email).IsUnique();
+
+            // Store phone number as string to minimize space
+            builder.Property(c => c.PhoneNumber).HasMaxLength(15);
+        }
+    }
+}
